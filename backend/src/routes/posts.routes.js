a7160ea008ac1acfd2ab1effaba2029a62668d78
Michelly-Oliveira <michellyneris.oliveira@gmail.com>
2020-07-20 14:30:01 -0300

    added likes and comments to post, created services folder to separate logic from the routes

--- backend/src/routes/posts.routes.js
+++ backend/src/routes/posts.routes.js
@@ -3,20 +3,25 @@ const { uuid } = require('uuidv4');
 
 const postsRouter = Router();
 
+const CreatePostService = require('../services/posts/CreatePostService');
+
 const posts = [];
 
-// POST http://localhost:3333/posts
+// http://localhost:3333/posts
 postsRouter.get('/', (request, response) => {
   return response.json(posts);
 });
 
+// User must be authenticated to be able to interact with posts: create a middleware to ensure the user is authenticated
 postsRouter.post('/', (request, response) => {
   const { content } = request.body;
 
-  const post = {
-    id: uuid(),
+  const createPost = new CreatePostService();
+
+  const post = createPost.execute({
+    user_id: 'whoWroteThePost_Id',
     content,
-  };
+  });
 
   posts.push(post);
 
@@ -33,14 +38,9 @@ postsRouter.put('/:id', (request, response) => {
     return response.status(400).json({ error: 'Could not find post' });
   }
 
-  const newPost = {
-    id,
-    content,
-  };
+  posts[findPostIndex].content = content;
 
-  posts[findPostIndex] = newPost;
-
-  return response.json(newPost);
+  return response.json(posts[findPostIndex]);
 });
 
 postsRouter.delete('/:id', (request, response) => {
@@ -55,6 +55,75 @@ postsRouter.delete('/:id', (request, response) => {
   posts.splice(findPostIndex, 1);
 
   return response.send({ msg: 'Post deleted' });
+});
+
+postsRouter.post('/:id/like', (request, response) => {
+  const { id } = request.params;
+
+  const findPost = posts.findIndex(post => post.id === id);
+
+  if (findPost < 0) {
+    return response.status(400).json({ error: 'Could not find post' });
+  }
+
+  posts[findPost].likes++;
+
+  return response.json(posts[findPost]);
+});
+
+postsRouter.delete('/:id/like', (request, response) => {
+  const { id } = request.params;
+
+  const findPost = posts.findIndex(post => post.id === id);
+
+  if (findPost < 0) {
+    return response.status(400).json({ error: 'Could not find post' });
+  }
+
+  if (posts[findPost].likes > 0) {
+    posts[findPost].likes--;
+  }
+
+  return response.json(posts[findPost]);
+});
+
+postsRouter.post('/:id/comment', (request, response) => {
+  const { id } = request.params;
+  const { content } = request.body;
+
+  const findPost = posts.findIndex(post => post.id === id);
+
+  if (findPost < 0) {
+    return response.status(400).json({ error: 'Could not find post' });
+  }
+
+  const comment = {
+    id: uuid(),
+    content,
+    author: 'Who wrote the comment',
+  };
+
+  posts[findPost].comments.push(comment);
+
+  return response.json(posts[findPost]);
+});
+
+postsRouter.delete('/:id/comment/:comment_id', (request, response) => {
+  const { id, comment_id } = request.params;
+
+  const findPost = posts.findIndex(post => post.id === id);
+
+  if (findPost < 0) {
+    return response.status(400).json({ error: 'Could not find post' });
+  }
+
+  const findComment = posts[findPost].comments.findIndex(
+    comment => comment.id === comment_id,
+  );
+
+  posts[findPost].comments.splice(findComment, 1);
+
+  return response.send({ msg: 'Comment removed' });
 });
 
 module.exports = postsRouter;
---
+++ backend/src/services/posts/CreatePostService.js
@@ -0,0 +1,69 @@
+const { uuid } = require('uuidv4');
+
+class CreatePostService {
+  execute({ user_id, content }) {
+    const date = getCreationDate();
+
+    const post = {
+      id: uuid(),
+      content,
+      date,
+      comments: [],
+      likes: 0,
+      user_id,
+    };
+
+    return post;
+  }
+}
+
+function getCreationDate() {
+  const date = new Date();
+
+  const dayOfMonth = date.getDate();
+  const numberOfMonth = date.getMonth();
+  let month;
+
+  switch (numberOfMonth) {
+    case 0:
+      month = 'Janeiro';
+      break;
+    case 1:
+      month = 'Fevereiro';
+      break;
+    case 2:
+      month = 'Março';
+      break;
+    case 3:
+      month = 'Abril';
+      break;
+    case 4:
+      month = 'Maio';
+      break;
+    case 5:
+      month = 'Junho';
+      break;
+    case 6:
+      month = 'Julho';
+      break;
+    case 7:
+      month = 'Agosto';
+      break;
+    case 8:
+      month = 'Setembro';
+      break;
+    case 9:
+      month = 'Outubro';
+      break;
+    case 10:
+      month = 'Novembro';
+      break;
+    case 11:
+      month = 'Dezembro';
+      break;
+  }
+
+  return `${dayOfMonth} de ${month}`;
+}
+
+module.exports = CreatePostService;
